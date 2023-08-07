20b7854175e6ce781d951ed075ab8e4b656c1a1c
Annah Nokutheza Mwanza <annahmkhwanazi18@gmail.com>
2023-08-07 12:51:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,21 +3,26 @@ const categorySelect = document.getElementById("categorySelect");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 const darkModeToggle = document.getElementById("darkModeToggle");
+const voiceRecordBtn = document.getElementById("voiceRecordBtn");
 
 addTaskBtn.addEventListener("click", addTask);
 taskList.addEventListener("click", deleteTask);
 darkModeToggle.addEventListener("change", toggleDarkMode);
+voiceRecordBtn.addEventListener("click", startVoiceRecording);
 
 function addTask() {
   const taskText = taskInput.value.trim();
   const category = categorySelect.value;
+  const dueDate = document.getElementById("dueDate").value;
 
   if (taskText !== "") {
     const li = document.createElement("li");
     li.innerHTML = `
       <span>${taskText}</span>
       <span class="category">${category}</span>
+      <span class="due-date">${dueDate}</span>
       <span class="delete-btn">Delete</span>
+      <input type="checkbox" class="task-completed">
     `;
     taskList.appendChild(li);
     taskInput.value = "";
@@ -35,3 +40,16 @@ function toggleDarkMode() {
   document.body.classList.toggle("dark-mode");
   document.querySelector(".container").classList.toggle("dark-mode");
 }
+
+function startVoiceRecording() {
+
+}
+
+// Data Visualization: Task Statistics
+function visualizeTaskStatistics() {
+  // Code to collect task data and create charts or graphs for data visualization
+  // You can use libraries like Chart.js or D3.js to create visual representations.
+}
+
+// Call the function to visualize task statistics (e.g., on page load)
+visualizeTaskStatistics();
